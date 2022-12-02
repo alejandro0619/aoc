1f701861f897677ae662ca893cd66e2a38a228cc
Spaghetti <alejandrolpz0619@gmail.com>
2022-12-02 15:30:26 -0400

    01 day

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn part_one() -> u32 {
+    let input = fs::read_to_string("input.txt").expect("Cannot find the file");
+
+    //println!("text: \n {}", input);
+
+    input
+        .split("\n\n")
+        .map(|line| {
+            line.lines()
+                .map(|cal| cal.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap()
+}
+
+fn part_two() {
+    let input = fs::read_to_string("input.txt").expect("Cannot find the file");
+
+    //println!("text: \n {}", input);
+
+    let mut input = input
+        .split("\n\n")
+        .map(|line| {
+            line.lines()
+                .map(|cal| cal.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+    input.sort_by(|a, b| b.cmp(a));
+    let sum = input.iter().take(3).sum::<u32>();
+    println!("{}", sum);
+}
+fn main() {
+    part_one();
+    part_two();
+}
