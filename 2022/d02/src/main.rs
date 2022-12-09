6acaa69b649e017211157eddfa61a3e413700b8b
Spaghetti <alejandrolpz0619@gmail.com>
2022-12-08 22:57:30 -0400

    part 2 day 2

--- 2022/d02/src/main.rs
+++ 2022/d02/src/main.rs
@@ -1,12 +1,13 @@
+use std::cmp;
 use std::fs;
-use std::cmp::Ordering;
+use std::str::FromStr;
 
-fn part_one() -> i32 {
+fn _part_one() -> i32 {
     let input = fs::read_to_string("input.txt").expect("Cannot find the file");
     let lines = input.lines();
 
     // we set their discriminant
-    #[derive(Debug, PartialEq)]
+    #[derive(PartialEq)]
     enum Shapes {
         Rock = 1,
         Paper = 2,
@@ -14,7 +15,7 @@ fn part_one() -> i32 {
     }
 
     impl PartialOrd for Shapes {
-        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
             // We only compare Rock an Scissors manually
             // Because Rock > Scissors and viceversa Scissors < Rock
             // So if we compare it within its discriminant
@@ -27,9 +28,9 @@ fn part_one() -> i32 {
             // 2 < 3 Scissors beats Paper
 
             if *self == Shapes::Rock && *other == Shapes::Scissors {
-                Some(Ordering::Greater)
+                Some(cmp::Ordering::Greater)
             } else if *self == Shapes::Scissors && *other == Shapes::Rock {
-                Some(Ordering::Less)
+                Some(cmp::Ordering::Less)
             } else {
                 Some((*self as i32).cmp(&(*other as i32)))
             }
@@ -50,16 +51,96 @@ fn part_one() -> i32 {
                 .collect::<Vec<Shapes>>()
         })
         .fold(0, |acc, mov| {
-            if mov[0] > mov [1] {
-                acc + mov [1] as i32 + 0
-            } else if mov [0] < mov [1] {
-                acc + mov [1] as i32 + 6
+            if mov[0] > mov[1] {
+                acc + mov[1] as i32 + 0
+            } else if mov[0] < mov[1] {
+                acc + mov[1] as i32 + 6
             } else {
-                acc + mov [1] as i32 + 3
+                acc + mov[1] as i32 + 3
             }
         })
+}
 
+fn part_two() {
+    let input = fs::read_to_string("input.txt").expect("Cannot find the file");
+    let lines = input.lines();
+
+    #[derive(Debug)]
+    enum Shapes {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3,
+        Win,
+        Draw,
+        Lose,
+    }
+
+    impl Shapes {
+        fn win(&self) -> Self {
+            match *self {
+                Shapes::Rock => Shapes::Paper,
+                Shapes::Paper => Shapes::Scissors,
+                Shapes::Scissors => Shapes::Rock,
+                _ => unreachable!(),
+            }
+        }
+
+        fn lose(&self) -> Self {
+            match *self {
+                Shapes::Rock => Shapes::Scissors,
+                Shapes::Paper => Shapes::Rock,
+                Shapes::Scissors => Shapes::Paper,
+                _ => unreachable!(),
+            }
+        }
+        fn draw(&self) -> Self {
+            match *self {
+                Shapes::Rock => Shapes::Rock,
+                Shapes::Paper => Shapes::Paper,
+                Shapes::Scissors => Shapes::Scissors,
+                _ => unreachable!(),
+            }
+        }
+    }
+    impl FromStr for Shapes {
+        type Err = &'static str;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "A" => Ok(Self::Rock),
+                "B" => Ok(Self::Paper),
+                "C" => Ok(Self::Scissors),
+                "X" => Ok(Self::Lose),
+                "Y" => Ok(Self::Draw),
+                "Z" => Ok(Self::Win),
+                _ => Err("Not a valid move"),
+            }
+        }
+    }
+
+    let result: i32 = lines
+        .map(|item| {
+            item.split_whitespace()
+                .map(|movement| {
+                        match movement {
+                        "A" | "B" | "C" => movement.parse::<Shapes>().unwrap(),
+                        "X" | "Y" | "Z" => movement.parse::<Shapes>().unwrap(),
+                        _ => unreachable!(),
+                    }
+                })
+                .collect::<Vec<Shapes>>()
+        }).map(|item| {
+            match item[1] {
+                Shapes::Lose => item[0].lose() as i32 + 0,
+                Shapes::Draw => item[0].draw() as i32 + 3,
+                Shapes::Win =>  item[0].win() as i32 + 6,
+                _ => unreachable!(),
+            }
+        }).sum();
+
+        println!("{}", result);
 }
 fn main() {
-    println!("{:?}", part_one());
+    //println!("{:?}", part_one());
+    println!("{:?}", part_two());
 }
