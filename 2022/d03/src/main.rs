1a1f28a132f1c2b4e529a904097efd3aeb84e021
Spaghetti <alejandrolpz0619@gmail.com>
2022-12-10 21:54:13 -0400

    Day 3 part 2 completed input.txt isn't included anymore

--- 2022/d03/src/main.rs
+++ 2022/d03/src/main.rs
@@ -1,15 +1,15 @@
-use std::{fs, collections::HashMap};
+#![feature(iter_array_chunks)]
 
-fn part_one(input: String) -> () {
+use std::{collections::HashMap, fs};
+
+fn part_one(input: &String) -> () {
     let lines = input.lines();
 
     let letters_scores = ('a'..='z')
-    .chain('A'..='Z')
-    .enumerate()
-    .map(|(i, l)|{
-        (l, i + 1)
-    })
-    .collect::<HashMap<char, usize>>();
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(i, l)| (l, i + 1))
+        .collect::<HashMap<char, usize>>();
 
     #[derive(Debug)]
     struct Compartments {
@@ -28,18 +28,53 @@ fn part_one(input: String) -> () {
         })
         .map(|item| -> i32 {
             let common = item
-            .first
-            .chars()
-            .find(|c| item.second.contains(*c)).unwrap();
+                .first
+                .chars()
+                .find(|c| item.second.contains(*c))
+                .unwrap();
 
             *letters_scores.get(&common).unwrap() as i32
         })
         .sum::<i32>();
     println!("{}", score);
-    
 }
+fn part_two(input: &String) {
+    let letters_scores = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(i, l)| (l, i + 1))
+        .collect::<HashMap<char, usize>>();
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct RucksackGroup {
+        first: String,
+        second: String,
+        third: String,
+    }
+    let total_score = input
+        .lines()
+        .array_chunks::<3>()
+        .map(|[f, s, t]| RucksackGroup {
+            first: f.to_string(),
+            second: s.to_string(),
+            third: t.to_string(),
+        })
+        .map(|item| {
+            let common = item
+                .first
+                .chars()
+                .find(|c| item.second.contains(*c) && item.third.contains(*c))
+                .unwrap();
 
+            *letters_scores.get(&common).unwrap() as i32
+        })
+        .sum::<i32>();
+
+    println!("{}", total_score)
+}
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Can't find the file");
-    part_one(input);
+    part_one(&input);
+
+    part_two(&input);
 }
