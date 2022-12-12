45fcecc16a8b98ccee2f4684d124774a75586626
Spaghetti <alejandrolpz0619@gmail.com>
2022-12-11 23:00:15 -0400

    day 4 part 1

--- 2022/d04/src/main.rs
+++ 2022/d04/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn part_one(input: &String) -> u32 {
+    input
+        .lines()
+        .map(|line: &str| {
+            line.split(",")
+                .map(|pairs| {
+                    pairs
+                        .split("-")
+                        .map(|sections| sections.parse::<u32>().unwrap())
+                        .collect::<Vec<u32>>()
+                })
+                .collect::<Vec<Vec<u32>>>()
+        })
+        .map(|item | {
+                if (item[0][0] <= item[1][0] && item[0][1] >= item[1][1]) || item[0][0] >= item[1][0] && item[0][1] <= item[1][1] {
+                    1
+                } else {
+                  0
+                }
+            },
+        )
+      .sum::<u32>()
 }
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not find the file");
+    println!("{}", part_one(&input))
+}
+
